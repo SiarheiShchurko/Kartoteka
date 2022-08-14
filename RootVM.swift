85453a2603bae278835c1801170f365eed2febc8
Siarhei Shchurko <101253596+SiarheiShchurko@users.noreply.github.com>
2022-08-14 18:16:39 +0300

    Add files via upload

---
+++ RootVM.swift
@@ -0,0 +1,19 @@
+//
+//  RootVM.swift
+//  Kartoteka.by
+//
+//  Created by Alinser Shchurko on 12.08.22.
+//
+
+import Foundation
+
+protocol RootProtokol {
+    static var unp: Int? { get set }
+}
+
+
+class RootVM: RootProtokol {
+    
+    static var unp: Int?
+
+}
