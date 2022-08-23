637aab19e4fd72e08a14eef232ce7e47aadbad03
SiarheiShchurko <Pridemc@yandex.ru>
2022-08-23 16:36:28 +0300

    Created VM for history views display.

--- KartotekaApp (Project)/VC/ModalsVC/HistoryViewsCV/HistoryViewsVC.swift
+++ KartotekaApp (Project)/VC/ModalsVC/HistoryViewsCV/HistoryViewsVC.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 class HistoryViewsVC: UIViewController {
+    
+    var historyVM: HistoryViewsProtocol = HistoryVM()
+    
     var rootViewModel: RootProtokol = RootVM()
-    var cell = HistoryViewTableVCell()
+    
     
     @IBOutlet private weak var titleHistoryLabel: UILabel!
     @IBOutlet private weak var headerView: UIView! {
@@ -21,49 +24,36 @@ class HistoryViewsVC: UIViewController {
         }
     }
     
-    private var client: [History] = []
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        showClients()
+        historyVM.update = { self.tableView.reloadData() }
+        historyVM.showClients()
         
     }
-    //MARK - Open clients
+    //MARK: - Open clients
     func open(_ inf: History) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
          guard let nextVC = storyboard.instantiateViewController(withIdentifier: "\(SearchResultVC.self)") as? SearchResultVC else { return }
         present(UINavigationController(rootViewController: nextVC), animated: true)
     }
-    
-    
-    //MARK: func Sort list clients in DataBase
-    
-    private func showClients() {
-        let request = History.fetchRequest()
-        request.sortDescriptors = [NSSortDescriptor(key: #keyPath(History.date), ascending: false)]
-        let client = try? HistoryCrDtService.mainContext.fetch(request)
-        self.client = client ?? []
-        tableView.reloadData()
-    }
 }
 
 extension HistoryViewsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        client.count
+        historyVM.client.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "\(HistoryViewTableVCell.self)", for: indexPath) as? HistoryViewTableVCell
-        cell?.setUpForListInDataBase(client[indexPath.row])
+        cell?.setUpForListInDataBase(historyVM.client[indexPath.row])
         return cell ?? .init()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if let unp = Int(client[indexPath.row].unp ?? "0") {
+        if let unp = Int(historyVM.client[indexPath.row].unp ?? "0") {
             RootVM.unp = unp
         }
-        open(client[indexPath.row])
+        open(historyVM.client[indexPath.row])
     }
     
  
@@ -75,7 +65,7 @@ extension HistoryViewsVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let removedClient = client.remove(at: indexPath.row)
+            let removedClient = historyVM.client.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
             HistoryCrDtService.mainContext.delete(removedClient)
             HistoryCrDtService.saveContext()
---
+++ KartotekaApp (Project)/VC/ModalsVC/HistoryViewsCV/HistoryViewsVM.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryVM.swift
+//  Kartoteka.by
+//
+//  Created by Alinser Shchurko on 23.08.22.
+//
+
+import Foundation
+import UIKit
+protocol HistoryViewsProtocol {
+    var client: [History] { get set }
+    var update: (() -> Void)? { get set }
+    func showClients()
+}
+
+final class HistoryVM: HistoryViewsProtocol {
+    
+    var client: [History] = []
+    
+    var update: (() -> Void)?
+    
+    //MARK: func Sort list clients in DataBase
+    func showClients() {
+         let request = History.fetchRequest()
+         request.sortDescriptors = [NSSortDescriptor(key: #keyPath(History.date), ascending: false)]
+         let client = try? HistoryCrDtService.mainContext.fetch(request)
+         self.client = client ?? []
+         self.update?()
+     }
+}
