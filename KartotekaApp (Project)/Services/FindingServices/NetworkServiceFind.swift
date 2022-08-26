e17648e4966fcc02102136753997a17498431069
SiarheiShchurko <Pridemc@yandex.ru>
2022-08-26 15:33:59 +0300

    Work with GCD. Add both network services run to global thread.

--- KartotekaApp (Project)/Services/FindingServices/NetworkServiceFind.swift
+++ KartotekaApp (Project)/Services/FindingServices/NetworkServiceFind.swift
@@ -22,25 +22,28 @@ final class NetworkServiceFind {
     let dateFormatter = DateFormatter()
     
     func loadInfo(_ complition: @escaping (FullResponse) -> Void) {
-        if let unp = RootVM.unp {
-            guard let url = URL(string: "https://api.kartoteka.by/summary/\(unp)?access-token=\(token)") else { return }
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            session.dataTask(with: request) { [ weak self ] (responseData, response, error) in
-                guard let strongSelf = self else { return }
-                print(response ?? "Response clear")
-                if let error = error {
-                    print(error.localizedDescription)
-                } else if let data = responseData, error == nil {
-                    self?.dateFormatter.dateFormat = "yyyy-MM-dd"
-                    self?.decoder.dateDecodingStrategy = .formatted(self?.dateFormatter ?? DateFormatter())
-                    guard let infoEGR = try? strongSelf.decoder.decode(FullResponse.self, from: data) else { return }
-                        DispatchQueue.main.async {
-                            complition(infoEGR)
-                        }
-                }
-            }.resume()
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let unp = RootVM.unp {
+                guard let url = URL(string: "https://api.kartoteka.by/summary/\(unp)?access-token=\(self.token)") else { return }
+                var request = URLRequest(url: url)
+                request.httpMethod = "GET"
+                self.session.dataTask(with: request) { [ weak self ] (responseData, response, error) in
+                    guard let strongSelf = self else { return }
+                    print(response ?? "Response clear")
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else if let data = responseData, error == nil {
+                        self?.dateFormatter.dateFormat = "yyyy-MM-dd"
+                        self?.decoder.dateDecodingStrategy = .formatted(self?.dateFormatter ?? DateFormatter())
+                        guard let infoEGR = try? strongSelf.decoder.decode(FullResponse.self, from: data) else { return }
+                            DispatchQueue.main.async {
+                                complition(infoEGR)
+                            }
+                    }
+                }.resume()
+            }
         }
+        
     }
 }
 
--- KartotekaApp (Project)/Services/SearchResultServices/NetworkService.swift
+++ KartotekaApp (Project)/Services/SearchResultServices/NetworkService.swift
@@ -26,31 +26,34 @@ class NetworkService {
     let dateFormatter = DateFormatter()
     
     func loadInfo(_ complition: @escaping (ObtainClientsInform) -> Void) {
-    if let unp = RootVM.unp {
-            print("Look at me now \(unp)")
-            var result: ObtainClientsInform = .outOfStock
-            guard let url = URL(string: "https://api.kartoteka.by/egr/\(unp)?access-token=\(token)") else { return }
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            session.dataTask(with: request) { [ weak self ] (responseData, response, error) in
-                
-                guard let strongSelf = self else { return }
-                print(response ?? "Response clear")
-                if let error = error {
-                    print(error.localizedDescription)
-                } else if let data = responseData, error == nil {
-                    self?.dateFormatter.dateFormat = "yyyy-MM-dd"
-                    self?.decoder.dateDecodingStrategy = .formatted(self?.dateFormatter ?? DateFormatter())
-                    do {
-                        guard let infoEGR = try? strongSelf.decoder.decode(EgrResponse.self, from: data) else { return }
-                            result = .success(inf: infoEGR)
-                        DispatchQueue.main.async {
-                            complition(result) }
-                        HistoryCrDtService.saveDataBaseMethod(infoEGR)
-               }
-}
-            }.resume()
+        DispatchQueue.global(qos: .userInitiated).async {
+            if let unp = RootVM.unp {
+                    print("Look at me now \(unp)")
+                    var result: ObtainClientsInform = .outOfStock
+                    guard let url = URL(string: "https://api.kartoteka.by/egr/\(unp)?access-token=\(self.token)") else { return }
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "GET"
+                    self.session.dataTask(with: request) { [ weak self ] (responseData, response, error) in
+                        
+                        guard let strongSelf = self else { return }
+                        print(response ?? "Response clear")
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else if let data = responseData, error == nil {
+                            self?.dateFormatter.dateFormat = "yyyy-MM-dd"
+                            self?.decoder.dateDecodingStrategy = .formatted(self?.dateFormatter ?? DateFormatter())
+                            do {
+                                guard let infoEGR = try? strongSelf.decoder.decode(EgrResponse.self, from: data) else { return }
+                                    result = .success(inf: infoEGR)
+                                DispatchQueue.main.async {
+                                    complition(result) }
+                                HistoryCrDtService.saveDataBaseMethod(infoEGR)
+                       }
         }
+                    }.resume()
+                }
+        }
+    
     }
 }
     
